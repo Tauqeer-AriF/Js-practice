b6254d91b383f0a1263f905df571376104fef10d
Tauqeer-AriF <chtauqeer1117@gmail.com>
2020-03-11 10:42:19 -0700

    js added

---
+++ Practice work 1/js.js
@@ -0,0 +1,18 @@
+
+  
+			function showHideDiv(ele) {
+				var srcElement = document.getElementById(ele);
+					if (srcElement.style.display == "block") {
+						srcElement.style.display = 'none';
+					}
+					else {
+						srcElement.style.display = 'block';
+					}
+					return false;
+				}
+
+		function showtext() {
+		document.getElementById('practice').style.display='block';
+		}
+
+		
